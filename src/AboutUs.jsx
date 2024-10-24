3f7cfc2d9208942209c244144189d2929f9aa023
Panu20 <paul.nunez2027@gmail.com>
2024-10-24 11:38:22 -0400

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './AboutUs.css';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CreateSlice';
 
 function AboutUs() {
+  
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Paul's Paradise Nursery, where green meets serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
+        At Paul's Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
         high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
         more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
         plant enthusiast.
